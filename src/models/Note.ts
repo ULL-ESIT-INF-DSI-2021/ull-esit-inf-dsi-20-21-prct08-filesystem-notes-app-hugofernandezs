deade5ebc8868329220ffcb5ce59e2ad415461bd
Hugo Fernández Solís <alu0101112664@ull.edu.es>
2021-04-25 12:05:27 +0100

    Añadida la clase Note

---
+++ src/models/Note.ts
@@ -0,0 +1,27 @@
+
+
+export const enum Color {
+  red = "Red",
+  yellow = "Yellow",
+  green = "Green",
+  blue = "Blue",
+};
+
+
+/**
+ * Implements a user note.
+ * @var title Title of the note.
+ * @var color Color of the note.
+ * @var body Body of the note_
+ */
+export class Note {
+  public title: string;
+  public color: Color;
+  public body: string;
+
+  constructor(newTitle: string, newColor: Color, newBody: string) {
+    this.title = newTitle;
+    this.color = newColor;
+    this.body = newBody;
+  }
+}
